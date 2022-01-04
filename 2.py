050afeda3b29f8c8b755ebdf6a46be707671ae76
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2022-01-03 22:07:21 -0800

    Code clean up, solution accepted

--- 2.py
+++ 2.py
@@ -6,46 +6,24 @@
 class Solution:
 	def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
 		# This is just simple addition. 
-		sum_list = []
+		sum_list_head = ListNode(None)
+		sum_linked_list = sum_list_head
 		quotient = 0
 
-		node_1 = l1
-		node_2 = l2
-
-		while node_1 is not None or node_2 is not None:
-			if node_1 is None:
-				a = 0
-			else: 
-				a = node_1.val
-			
-			if node_2 is None:
-				b = 0
-			else: 
-				b = node_2.val
+		while l1 or l2:
+			a = 0 if l1 is None else l1.val
+			b = 0 if l2 is None else l2.val
 
 			quotient, remainder = divmod(quotient + a + b, 10)
-			sum_list.append(remainder)
+			sum_linked_list.next = ListNode(remainder)
+			sum_linked_list = sum_linked_list.next
 			
 			# Next node
-			if node_1 is None:
-				node_1 = None
-			else:
-				node_1 = node_1.next
-			
-			if node_2 is None:
-				node_2 = None
-			else:
-				node_2 = node_2.next
+			l1 = None if l1 is None else l1.next
+			l2 = None if l2 is None else l2.next
 		
 		if quotient > 0:
 			# Tack on a 1 if it carries over at the end
-			sum_list.append(quotient)
+			sum_linked_list.next = ListNode(quotient)
 		
-		# Format output, create linked list
-		# Reverse list
-		sum_list.reverse()
-		sum_linked_list = None
-		for i in range(len(sum_list)):
-			sum_linked_list = ListNode(val = sum_list[i], next = sum_linked_list)
-		
-		return sum_linked_list
+		return sum_list_head.next
