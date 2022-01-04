b268c759036839d19ad87e9eadd53d53e879d734
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2022-01-04 13:48:07 -0800

    Fixed edge case, improved speed

--- 5.py
+++ 5.py
@@ -6,19 +6,29 @@ class Solution:
 		longest_pal = ""
 
 		for i in range(len(s) * 2):
-			left_index = math.floor(i / 2)
-			right_index = math.ceil(i / 2)
+			len_longest_pal = len(longest_pal)
+			left_index = math.floor(i / 2) - len_longest_pal // 2
+			right_index = math.ceil(i / 2) + len_longest_pal // 2
 			test_str = s[left_index : right_index + 1]
-			while test_str[0] == test_str[-1]:
-				if len(test_str) > len(longest_pal):
-					longest_pal = test_str
-				
-				# Check next
-				left_index -= 1
-				right_index += 1
-				if left_index >= 0 and right_index < len(s):
-					test_str = s[left_index : right_index + 1]
-				else:
-					break
+			if self.isPal(test_str):
+				while test_str[0] == test_str[-1]:
+					if len(test_str) > len(longest_pal):
+						longest_pal = test_str
+					
+					# Check next
+					left_index -= 1
+					right_index += 1
+					if left_index >= 0 and right_index < len(s):
+						test_str = s[left_index : right_index + 1]
+					else:
+						break
 		
 		return longest_pal
+
+	def isPal(self, s):
+		max_index = len(s) - 1
+		for i in range(len(s)):
+			if s[i] != s[max_index - i]:
+				return False
+		
+		return True
