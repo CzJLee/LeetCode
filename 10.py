673e81bc2ce156f1462053f78d0eb1bfe8a0c2e9
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2022-01-04 17:47:15 -0800

    Recursion accepted

--- 10.py
+++ 10.py
@@ -1,3 +1,15 @@
 class Solution:
 	def isMatch(self, s: str, p: str) -> bool:
+		if not p:
+			return not s
+
+		# Recursive approach
+		first_char_match = bool(s) and (p[0] == s[0] or p[0] == ".")
 		
+		# Check if the pattern has a folowing * char
+		if len(p) >= 2 and p[1] == "*":
+			# Either keep matching text or Text does not match, move on
+			return (first_char_match and self.isMatch(s[1:], p)) or self.isMatch(s, p[2:])
+		else:
+			# No * to worry about, move on as normal
+			return first_char_match and self.isMatch(s[1:], p[1:])
