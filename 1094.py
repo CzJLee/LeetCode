7ca037d958974956a39fe480c3228fb578778422
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2022-01-06 11:34:31 -0800

    Optimized size

--- 1094.py
+++ 1094.py
@@ -7,20 +7,20 @@ class Solution:
 		num_trips = trips.shape[0]
 
 		# Rearrange format of trips so that it is easier to sequentially step through
-		stops = np.ones(shape = (num_trips * 2, 3), dtype=np.int32)
+		stops = np.ones(shape = (num_trips * 2, 2), dtype=np.int32)
 
 		# Add pickups and dropoffs
-		stops[:num_trips, :] = np.column_stack((trips[:, 0], trips[:, 1], np.ones(shape = num_trips))) # pickups
-		stops[num_trips:, :] = np.column_stack((trips[:, 0], trips[:, 2], -1 * np.ones(shape = num_trips))) # dropoffs
+		stops[:num_trips, :] = np.column_stack((trips[:, 0], trips[:, 1])) # pickups
+		stops[num_trips:, :] = np.column_stack((trips[:, 0] * -1, trips[:, 2])) # dropoffs
 
 		# Sort by location, with subsort sorting dropoffs first
 		# https://stackoverflow.com/questions/2828059/sorting-arrays-in-numpy-by-column
-		stops = stops[stops[:,2].argsort()] # First sort doesn't need to be stable.
+		stops = stops[stops[:,0].argsort()] # First sort doesn't need to be stable.
 		stops = stops[stops[:,1].argsort(kind='stablesort')]
 
 		for stop in stops:
 			# At every stop, reduce capacity if people get in, gain capacity if people leave.
-			capacity -= stop[0] * stop[2]
+			capacity -= stop[0]
 			# If capacity drops below zero, trip fails. 
 			if capacity < 0:
 				return False
