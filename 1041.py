5d31ef2696f8fee820a432372e1424ea9b99b898
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2022-01-09 14:38:56 -0800

    Solution Accepted

---
+++ 1041.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class Solution:
+	# I think you just have to check for symmetry. If the robot does not end up at the origin after four iterations, then I don't think it is possible for it to be bounded. 
+	def isRobotBounded(self, instructions: str) -> bool:
+		pos = np.array([0, 0])
+		dir = np.array([0, 1])
+		left = np.array([[0, -1], [1, 0]])
+		right = np.array([[0, 1], [-1, 0]])
+		instructions = instructions * 4
+
+		for inst in instructions:
+			if inst == "G":
+				pos = np.add(pos, dir)
+			elif inst == "L":
+				dir = np.matmul(left, dir)
+			elif inst == "R":
+				dir = np.matmul(right, dir)
+		
+		return np.array_equal(np.array([0, 0]), pos)
