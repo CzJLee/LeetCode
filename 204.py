5d6d9eef8b3fa050371836a136697bcef5bf4c1b
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2022-01-10 13:41:34 -0800

    Faster using np

--- 204.py
+++ 204.py
@@ -1,18 +1,14 @@
 # https://leetcode.com/problems/count-primes/
 import math
+import numpy as np
 class Solution:
 	def countPrimes(self, n: int) -> int:
 		if n <= 2:
 			return 0
 		# Create a sieve 
-		nums = list(range(n))
+		nums = np.arange(n, dtype=np.int32)
 		for i in range(2, int(math.sqrt(n)) + 1):
 			if nums[i] != 0:
-				j = i ** 2
-				while j < n:
-					nums[j] = 0
-					j += i
-		count = len(set(nums)) - 2
+				nums[i**2:n:i] = 0
+		count = len(np.unique(nums)) - 2
 		return count
-
-
