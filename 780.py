5ef2d658ec6fac533df9e97fb3dd8c0aebce5f53
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2022-01-11 09:14:15 -0800

    Mod variant time complexity speedup

--- 780.py
+++ 780.py
@@ -1,19 +1,20 @@
 import time
 # https://leetcode.com/problems/reaching-points/
 class Solution:
-	def reachingPoints(self, sx: int, sy: int, tx: int, ty: int) -> bool:
-		# Try a recursive search
-		if (sx, sy) == (tx, ty):
-			return True
-		elif sx > tx or sy > ty:
-			# Stopping point is if you exceed values
-			return False
-		else:
-			return self.reachingPoints(sx + sy, sy, tx, ty) or self.reachingPoints(sx, sx + sy, tx, ty)
-		
+	# Working backward, mod variant to speed up time complexity
+	def reachingPoints(self, sx, sy, tx, ty):
+		while tx >= sx and ty >= sy:
+			if tx == ty:
+				break
+			elif tx > ty:
+				if ty > sy:
+					tx %= ty
+				else:
+					return (tx - sx) % ty == 0
+			else:
+				if tx > sx:
+					ty %= tx
+				else:
+					return (ty - sy) % tx == 0
 
-if __name__ == "__main__":
-	sol = Solution()
-	start = time.time()
-	print(sol.reachingPoints(1, 2, 3135341, 11948575))
-	print(f"Time: {time.time() - start}")
+		return tx == sx and ty == sy
