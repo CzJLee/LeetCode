5db3993a63f342ef1d5537b0bc9bc4b9c01e7fd7
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2022-01-11 09:09:45 -0800

    Exceeds recursion

--- 780.py
+++ 780.py
@@ -1,3 +1,4 @@
+import time
 # https://leetcode.com/problems/reaching-points/
 class Solution:
 	def reachingPoints(self, sx: int, sy: int, tx: int, ty: int) -> bool:
@@ -10,3 +11,9 @@ class Solution:
 		else:
 			return self.reachingPoints(sx + sy, sy, tx, ty) or self.reachingPoints(sx, sx + sy, tx, ty)
 		
+
+if __name__ == "__main__":
+	sol = Solution()
+	start = time.time()
+	print(sol.reachingPoints(1, 2, 3135341, 11948575))
+	print(f"Time: {time.time() - start}")
