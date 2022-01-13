2116f2ec545d25d5e3fd03b4e250233f5e63ef71
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2022-01-13 10:39:42 -0800

    Better greedy sol

--- 452.py
+++ 452.py
@@ -1,46 +1,19 @@
 # https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
 class Solution:
-	def findMinArrowShots(self, points: List[List[int]]) -> int:
-		dart_count = 0 
-		points.sort()
-		while points:
-			print(points)
-			target_val = None
-			target_num_bloons = 0
-			# Get position with highest bloon pop count
-			for x1, x2 in points:
-				num_bloons_x1 = 0
-				num_bloons_x2 = 0
-				for point in points:
-					if x1 < point[0]:
-						break
-					if x1 <= point[1]:
-						num_bloons_x1 += 1
-				for point in points:
-					if x2 < point[0]:
-						break
-					if x2 <= point[1]:
-						num_bloons_x2 += 1
-				if num_bloons_x1 >= num_bloons_x2 and num_bloons_x1 > target_num_bloons:
-					target_val = x1
-					target_num_bloons = num_bloons_x1
-				elif num_bloons_x2 > num_bloons_x1 and num_bloons_x2 > target_num_bloons:
-					target_val = x2
-					target_num_bloons = num_bloons_x2
-			print(target_val)
-			print(target_num_bloons)
-			# Pop bloons
-			new_points = []
-			last_index = None
-			for i, point in enumerate(points):
-				if point[0] > target_val:
-					last_index = i
-					break
-				if point[1] < target_val:
-					new_points.append(point)
-			if last_index:
-				new_points.extend(points[last_index:])
-			points = new_points
-			dart_count += 1
-		return dart_count
-
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        if not points:
+            return 0
+        
+        # sort by x_end
+        points.sort(key = lambda x : x[1])
+        
+        arrows = 1
+        first_end = points[0][1]
+        for x_start, x_end in points:
+            # if the current balloon starts after the end of another one,
+            # one needs one more arrow
+            if first_end < x_start:
+                arrows += 1
+                first_end = x_end
+        
+        return arrows
