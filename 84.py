6588c3d9b9398470917f8df29f6708f82881c5bc
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2022-01-28 17:55:57 -0800

    Divide and conquer. Works, slow for flat hist

--- 84.py
+++ 84.py
@@ -1,44 +1,10 @@
 # https://leetcode.com/problems/largest-rectangle-in-histogram/
 class Solution:
 	def largestRectangleArea(self, heights: List[int]) -> int:
-		if len(heights) == 1:
-			return heights[0]
+		if len(heights) == 0:
+			return 0
 
-		areas = set()
-		all_heights = sorted(list(set(heights)))
-		heights = [list(heights)]
-		
-		# Start at bottom and rise up
-		def split(heights, value):
-			split_heights = []
-			segment = []
-			for x in heights:
-				for height in x:
-					if height > value:
-						segment.append(height)
-					elif height == value:
-						if len(segment) >= 1:
-							split_heights.append(segment)
-						segment = []
-				if len(segment) >= 1:
-					split_heights.append(segment)
-				segment = []
-			return split_heights
+		min_height = min(heights)
+		min_index = heights.index(min_height)
 
-		current_height = all_heights[0]
-		while len(all_heights) > 0:
-			# print(heights)
-			# Calculate all areas of segments
-			for segment in heights:
-				area = current_height * len(segment)
-				areas.add(area)
-			# Cut off bottom
-			heights = split(heights, current_height)
-			# Rise level
-			all_heights.pop(0)
-			if len(all_heights) > 0:
-				current_height = all_heights[0]
-		
-		return max(areas)
-
-
+		return max(min_height * len(heights), self.largestRectangleArea(heights[:min_index]), self.largestRectangleArea(heights[min_index+1:]))
