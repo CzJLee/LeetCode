08ff05b8a0eb747ff3720bb6c08366d45be6086f
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2022-01-28 17:56:47 -0800

    Stack solution is the only one fast enough

--- 84.py
+++ 84.py
@@ -1,10 +1,18 @@
 # https://leetcode.com/problems/largest-rectangle-in-histogram/
 class Solution:
-	def largestRectangleArea(self, heights: List[int]) -> int:
-		if len(heights) == 0:
-			return 0
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stack = [-1]
+        max_area = 0
+        for i in range(len(heights)):
+            while stack[-1] != -1 and heights[stack[-1]] >= heights[i]:
+                current_height = heights[stack.pop()]
+                current_width = i - stack[-1] - 1
+                max_area = max(max_area, current_height * current_width)
+            stack.append(i)
 
-		min_height = min(heights)
-		min_index = heights.index(min_height)
-
-		return max(min_height * len(heights), self.largestRectangleArea(heights[:min_index]), self.largestRectangleArea(heights[min_index+1:]))
+        while stack[-1] != -1:
+            current_height = heights[stack.pop()]
+            current_width = len(heights) - stack[-1] - 1
+            max_area = max(max_area, current_height * current_width)
+        return max_area
+ 
