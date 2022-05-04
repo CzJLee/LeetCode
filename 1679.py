4c575665263958eafe0ba7d9f2424730e6d56fca
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2022-05-03 18:04:29 -0700

    Explicit counter

--- 1679.py
+++ 1679.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/max-number-of-k-sum-pairs/
 
+from collections import Counter
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         cnt, ans = Counter(n for n in nums if n < k), 0
