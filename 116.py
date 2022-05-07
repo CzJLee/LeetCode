3f789c9e75a32b2ed9f8bfc0a0aadead3ebb31ce
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2022-05-07 13:01:35 -0700

    Accepted

---
+++ 116.py
@@ -0,0 +1,46 @@
+# https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        # BFS
+        # Encounter each layer in a row, shift node
+        
+        # Start by adding root, then None
+        # Then BFS
+        # When None is encountered, make it the right node, then add it back to the queue 
+        
+        if root is None:
+            return root
+        
+        queue = [root, None]
+        
+        node = queue.pop(0)
+        
+        while queue:
+            print(node)
+            if node is None:
+                if len(queue) > 0:
+                    queue.append(None)
+                    node = queue.pop(0)
+                continue
+            
+            if node.left is not None:
+                queue.append(node.left)
+                queue.append(node.right)
+                
+            next_node = queue.pop(0)
+            node.next = next_node
+            node = next_node
+        
+        return root
+                
