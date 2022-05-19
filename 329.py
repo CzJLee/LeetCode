42736b9aabc162b02ebf384e0512beecf63ff738
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2022-05-19 01:29:28 -0700

    Add faster sol

--- 329.py
+++ 329.py
@@ -116,3 +116,20 @@ matrix = [[75,26,75,88,8,6,38,34,20,80,91,96,44,86,65,81,84,73,72,17,50,92,80,31
 sol = Solution()
 
 print(sol.longestIncreasingPath(matrix))
+
+# Faster
+class Solution:
+    def longestIncreasingPath(self, matrix):
+        def dfs(i, j):
+            if not dp[i][j]:
+                val = matrix[i][j]
+                dp[i][j] = 1 + max(
+                    dfs(i - 1, j) if i and val > matrix[i - 1][j] else 0,
+                    dfs(i + 1, j) if i < M - 1 and val > matrix[i + 1][j] else 0,
+                    dfs(i, j - 1) if j and val > matrix[i][j - 1] else 0,
+                    dfs(i, j + 1) if j < N - 1 and val > matrix[i][j + 1] else 0)
+            return dp[i][j]
+
+        M, N = len(matrix), len(matrix[0])
+        dp = [[0] * N for i in range(M)]
+        return max(dfs(x, y) for x in range(M) for y in range(N))
