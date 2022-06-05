1dae01ccb0c072efb5ed00115c19fa009938f2a7
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2022-06-05 08:47:30 -0700

    I like dict sol better

--- 52.py
+++ 52.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/n-queens-ii/
 # Use precomputed solution from 51
-sol_list = [1, 0, 0, 2, 10, 4, 40, 92, 352]
+num_sol = {1: 1, 2: 0, 3: 0, 4: 2, 5: 10, 6: 4, 7: 40, 8: 92, 9: 352}
 class Solution:
     def totalNQueens(self, n: int) -> int:
-        return sol_list[n-1]
+        return num_sol[n]
