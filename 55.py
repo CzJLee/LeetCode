13c566ecf912c724b616268697ff8e06e8aa4bb3
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2022-06-15 11:50:45 -0700

    Faster greedy

--- 55.py
+++ 55.py
@@ -9,6 +9,7 @@ import sys
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        # DP Tabular solution
         dp = [False for _ in nums]
         dp[0] = True
         
@@ -19,3 +20,23 @@ class Solution:
                     dp[i] = True
                     break
         return dp[-1]
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        # If nums does not contain zero, then it is always true
+        if 0 not in nums:
+            return True
+
+        # Greedy solution
+        steps_remaining = 0
+
+        for n in nums:
+            # Base case
+            if steps_remaining < 0:
+                return False
+            # Greedy: if larger step is avl, land here and take it
+            steps_remaining = max(n, steps_remaining)
+            # Take step to next spot
+            steps_remaining -= 1
+        # Reached the end
+        return True
