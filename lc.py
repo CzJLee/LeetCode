d1747cad18dd70b68840e0f784226881d94d2046
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2022-06-15 10:45:25 -0700

    Ad test case running

--- lc.py
+++ lc.py
@@ -1,6 +1,10 @@
 from typing import Any, Optional, Tuple, Union, Iterable, Mapping, Counter, Dict, List, Set
 from functools import cache
 from itertools import product, permutations, combinations, combinations_with_replacement
+from collections import ChainMap, Counter, OrderedDict, defaultdict, deque, namedtuple
 import math
 import bisect
-from collections import ChainMap, Counter, OrderedDict, defaultdict, deque, namedtuple
+import sys
+
+if __name__ == "__main__" and sys.platform == "darwin":
+    print("Hello World.")
