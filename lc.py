88b2872adae728ebb30827d38fa14134e9301898
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2022-06-15 10:32:01 -0700

    Create default imports

---
+++ lc.py
@@ -0,0 +1,6 @@
+from typing import Any, Optional, Tuple, Union, Iterable, Mapping, Counter, Dict, List, Set
+from functools import cache
+from itertools import product, permutations, combinations, combinations_with_replacement
+import math
+import bisect
+from collections import ChainMap, Counter, OrderedDict, defaultdict, deque, namedtuple
