e059f15583c8b514ba95b0ccc5dbe3a0bdaa654a
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2024-05-06 16:36:50 -0700

    2487 first try

---
+++ 2487.py
@@ -0,0 +1,54 @@
+"""
+You are given the head of a linked list.
+
+Remove every node which has a node with a greater value anywhere to the right side of it.
+
+Return the head of the modified linked list.
+
+ 
+
+Example 1:
+
+
+Input: head = [5,2,13,3,8]
+Output: [13,8]
+Explanation: The nodes that should be removed are 5, 2 and 3.
+- Node 13 is to the right of node 5.
+- Node 13 is to the right of node 2.
+- Node 8 is to the right of node 3.
+Example 2:
+
+Input: head = [1,1,1,1]
+Output: [1,1,1,1]
+Explanation: Every node has value 1, so no nodes are removed.
+ 
+
+Constraints:
+
+The number of the nodes in the given list is in the range [1, 105].
+1 <= Node.val <= 105
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """Use recursion to get LL in reverse order, descending."""
+
+        def decreasing_nodes(node):
+            if node.next is None:
+                return node
+            
+            next_node = decreasing_nodes(node.next)
+            if next_node.val <= node.val:
+                # Keep this node
+                node.next = next_node
+                return node
+            else:
+                # Remove this node
+                return next_node
+        
+        return decreasing_nodes(head)
