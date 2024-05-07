4bb2327687c57a6abfd266180ee176dbe1297a90
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2024-05-07 10:30:51 -0700

    2816 first try

---
+++ 2816.py
@@ -0,0 +1,55 @@
+"""
+You are given the head of a non-empty linked list representing a non-negative integer without leading zeroes.
+
+Return the head of the linked list after doubling it.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,8,9]
+Output: [3,7,8]
+Explanation: The figure above corresponds to the given linked list which represents the number 189. Hence, the returned linked list represents the number 189 * 2 = 378.
+Example 2:
+
+
+Input: head = [9,9,9]
+Output: [1,9,9,8]
+Explanation: The figure above corresponds to the given linked list which represents the number 999. Hence, the returned linked list reprersents the number 999 * 2 = 1998. 
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 104]
+0 <= Node.val <= 9
+The input is generated such that the list represents a number that does not have leading zeros, except the number 0 itself.
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        stack = []
+        node = head
+
+        while node:
+            stack.append(node)
+            node = node.next
+        
+        q = 0
+        while stack:
+            node = stack.pop()
+            new_val = node.val * 2 + q
+            q, r = divmod(new_val, 10)
+            node.val = r
+        
+        if not q:
+            return node
+        else:
+            return ListNode(val=q, next=node)
